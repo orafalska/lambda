b4b7adbcf1246a2529d244c7ce287ea2717143f6
olga_rafalska <olg.rafalsk@gmail.com>
2020-04-20 01:28:47 +0300

    Read csv from S3

--- demo/src/main/java/com/example/demo/SavePersonHandler.java
+++ demo/src/main/java/com/example/demo/SavePersonHandler.java
@@ -12,35 +12,44 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class SavePersonHandler implements RequestHandler<SQSEvent, PersonResponse> {
     private DynamoDB dynamoDb;
 
     private String TABLE_NAME = "persons";
+    private String S3_NAME = "my-s3-test-task";
+    private String S3_KEY = "s3.csv";
+
 
     public PersonResponse handleRequest(SQSEvent event, Context context) {
         LambdaLogger logger = context.getLogger();
-
-
-        logger.log("----------> Get new event" + event.toString());
-
         this.initDynamoDbClient();
 
         Gson gson = new Gson();
         PersonRequest personRequest;
 
-
         try {
             for (SQSEvent.SQSMessage message : event.getRecords()) {
                 String input = message.getBody();
-                logger.log("----------> Get new input" + input);
                 personRequest = gson.fromJson(input, PersonRequest.class);
                 persistData(personRequest);
             }
         } catch (Exception ex) {
             logger.log("Exception handling batch seed request.");
-            throw ex;
+            try {
+                throw ex;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         PersonResponse personResponse = new PersonResponse();
@@ -48,7 +57,7 @@ public class SavePersonHandler implements RequestHandler<SQSEvent, PersonRespons
         return personResponse;
     }
 
-    private PutItemOutcome persistData(PersonRequest personRequest) throws ConditionalCheckFailedException {
+    private PutItemOutcome persistData(PersonRequest personRequest) throws ConditionalCheckFailedException, IOException {
         String cityCSV;
         cityCSV = ReadCSV(personRequest.getId());
         if (!cityCSV.equals("") && !cityCSV.equals(null)) personRequest.setCity(cityCSV);
@@ -64,12 +73,25 @@ public class SavePersonHandler implements RequestHandler<SQSEvent, PersonRespons
                 .withString("lastName", personRequest.getLastName()).withString("city", personRequest.getCity())));
     }
 
-    public String ReadCSV(int id) {
-        return "Paris";
+    public String ReadCSV(int id) throws IOException {
+
+        AmazonS3 s3client = AmazonS3ClientBuilder
+                .standard()
+                .withRegion(Regions.EU_CENTRAL_1)
+                .build();
+        S3Object s3object = s3client.getObject(new GetObjectRequest(
+                S3_NAME, S3_KEY));
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(s3object.getObjectContent()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] cities = line.split(";");
+            if (Integer.parseInt(cities[0]) == id) return cities[1];
+        }
+        return "";
     }
 
     private void initDynamoDbClient() {
-
         AmazonDynamoDB ddb = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
         this.dynamoDb = new DynamoDB(ddb);
     }
